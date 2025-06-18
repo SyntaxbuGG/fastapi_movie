8d4feba21f08461930201bdeb68da7da4bbc1829
AZnepTuN <zarbotbekovabbosbek@gmail.com>
2025-06-19 01:33:30 +0500

    add origin url

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(
 
 app.add_middleware(
       CORSMiddleware,
-      allow_origins=["*"],
+      allow_origins=["90.156.161.97"],
       allow_credentials=True,
       allow_methods=["*"],
       allow_headers=["*"],
