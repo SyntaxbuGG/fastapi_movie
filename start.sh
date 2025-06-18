876de1676549dae0710ec8941222a4aa828daa7d
AZnepTuN <zarbotbekovabbosbek@gmail.com>
2025-06-18 23:07:35 +0500

    add build and start commands

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+set -o errexit
 
-# Применяем миграции Alembic
 alembic upgrade head
 
-# Запускаем FastAPI-приложение
 gunicorn app.main:app --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker
